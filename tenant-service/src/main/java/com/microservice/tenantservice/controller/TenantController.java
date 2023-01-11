4eba079b575306e3359ba739544fad8c303c1c98
M W <matwojciak@gmail.com>
2023-01-11 14:57:53 +0100

    Add docker files

--- tenant-service/src/main/java/com/microservice/tenantservice/controller/TenantController.java
+++ tenant-service/src/main/java/com/microservice/tenantservice/controller/TenantController.java
@@ -7,7 +7,6 @@ import com.microservice.tenantservice.service.TenantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
